2fcc4072d44bf54bc89a4ac0d926d61cb55c2447
AlChi1997 <alex.chi1997@hotmail.com>
2023-07-17 03:09:23 -0400

    finished sprint-2

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -10,69 +10,80 @@ let Shows = [
             {"DATE" : "Sat Dec 15 2021", "VENUE" : "Press Club", "LOCATION" :"San Francisco,CA"},
 ];
 
-//assigning the .main class as a variable
+//assigning the .footer class as a variable
 const footer =document.querySelector('footer');
 
+//assigning main as a new div and naming the div .main
 const main = document.createElement("div");
 main.classList.add("main");
 
+//assigning the body of the document as a parent and insert .main before the footer
 const body = footer.parentNode;
 body.insertBefore(main,footer);
 
-//assigning the name of variable Shows to be used as the title in main class
+//assigning the name of the object Shows (this took a while to search how to do)
 let mainHeader = Object.keys({Shows})[0];
 
-//creating a div element and assigning it to a variable divMain
+//creating a new div and assigning it the class name .main__header with the content of of name of the object Shows
 const divMain = document.createElement('div');
-//inserting mainHeader value into the created div
 divMain.textContent = mainHeader;
-//adding a class name to the created div
 divMain.classList.add("main__header");
-//adding the div onto the .main class as a child element
 main.appendChild(divMain);
 
 //conditional for script to run only when screen.width is <768px
 if (screen.width < 768){
 
+    //loop to go through each element of Shows object starting from the first index in this case 0
     for (let i = 0; i < Shows.length; i++) {
-        const obj = Shows[i];//this is to get the first object in the Shows array
-    
+        const obj = Shows[i];
+        
+        //creating a new div and assigning it the class name .main__content and appending it into .main to contain the keys and values divs
         const mainContent = document.createElement('div');
         mainContent.classList.add("main__content");
         main.appendChild(mainContent);
     
-        let isFirstValue = true;//we set a varaible as true so that the first run through it wil lbe true
+        let isFirstValue = true;//we set a varaible as true so that the first run through it will be true *see line 58 for reasoning
     
+            //loop to go through every key in the object Shows while creating a div .main__title within .main__content to contain each key
             for (let key in obj) {
             const title = document.createElement('div');
             title.textContent = key;
             title.classList.add("main__title");
             mainContent.appendChild(title);
         
+            //creating a new div and assignign it the class name .main__subtitle containing the value of each key
             const subtitle = document.createElement('div');
             subtitle.textContent = obj[key];
             subtitle.classList.add("main__subtitle");
-        
+            
+            //conditional to get the Date value in object as speical due to it being the only one highlighted, if so instead of .main__subtitle, we add .main__subtitle-bold
             if (isFirstValue) {
                 subtitle.classList.add("main__subtitle-bold");
-                isFirstValue = false; //after the first run through, we change the value to false
+                isFirstValue = false; //after the first run through, we change the value to false, this ensures we will not compute .main__subtitle-bold classes anymore
             }
-        
+            
+            //.main__subtitle is added into the .main__content div
             mainContent.appendChild(subtitle);
         }
 
+        //creating a button with the content "BUY TICKETS" and placing it within main__content
         let button = document.createElement('BUTTON');
         button.textContent = "BUY TICKETS";
         mainContent.appendChild(button);
     }
 }
+//conditional statement for scenarios where screen is >= tablet but less than desktop
 else if (screen.width >= 768 && screen.width < 1280){
+    
+    //created a new div to contain the keys as unlike mobile, keys only appear once named .main__subheader
     const mainSub = document.createElement('div');
     mainSub.classList.add("main__subheader");
     main.appendChild(mainSub);
 
+    //as we only need to go through the keys once, we only need 1 index of Shows
     const obj = Shows[0];
 
+    //loop to insert key into .main__title div within the newly created .main__subheader div
     for (let key in obj){
     const title = document.createElement('div');
     title.textContent = key;
@@ -80,6 +91,7 @@ else if (screen.width >= 768 && screen.width < 1280){
     mainSub.appendChild(title);
     }
 
+    //normal loop like before for values
     for (let i = 0; i < Shows.length; i++) {
         const obj = Shows[i];//this is to start on the first object in the Shows array
     
@@ -96,7 +108,7 @@ else if (screen.width >= 768 && screen.width < 1280){
             
                 if (isFirstValue) {
                     subtitle.classList.add("main__subtitle-bold");
-                    isFirstValue = false; //after the first run through, we change the value to false
+                    isFirstValue = false;
                 }
             
                 mainContent.appendChild(subtitle);
@@ -107,10 +119,12 @@ else if (screen.width >= 768 && screen.width < 1280){
     }
 }
 else{
+    //creating a new div called .main__container to contain all the right elements while styling for desktop
     const mainContain = document.createElement('div');
     mainContain.classList.add("main__container");
     main.appendChild(mainContain);
 
+    //same as before, created a div to contain the keys of Shows, but this time we add this div into the newly created .main__container
     const mainSub = document.createElement('div');
     mainSub.classList.add("main__subheader");
     mainContain.appendChild(mainSub);
@@ -124,6 +138,7 @@ else{
     mainSub.appendChild(title);
     }
 
+    //same loop as before for values, difference is instead of .main__content, we are inserting the divs into .main__container
     for (let i = 0; i < Shows.length; i++) {
         const obj = Shows[i];//this is to start on the first object in the Shows array
     
@@ -140,7 +155,7 @@ else{
             
                 if (isFirstValue) {
                     subtitle.classList.add("main__subtitle-bold");
-                    isFirstValue = false; //after the first run through, we change the value to false
+                    isFirstValue = false;
                 }
             
                 mainContent.appendChild(subtitle);
@@ -151,19 +166,26 @@ else{
     }
 }
 
+//assigning class .main__content to a variable highlightDiv
 let highlightDiv = document.querySelectorAll(".main__content");
+
+//assigning null to varaible null as to start with, there will be no divs highlighted
 let highlighted = null;
 
+//event listener function checking each element of class .main__content if it is highlighted
 highlightDiv.forEach((element)=>{
     element.addEventListener('click', (event)=>{
+        //conditional if highlighted doesn't = null which means highlighted = element, which means there is something highlighted, change the background of element back
         if(highlighted !== null){
             highlighted.style.backgroundColor = "";
         }
-
+        //conditional if element doesn't = null, change the element's background color to #E1E1E1 while changing highlighted to element, thus when it goes back to conditional 1, background of element will change back
         if(highlighted !== element){
             element.style.backgroundColor = "#E1E1E1";
             highlighted = element;
-        }else{
+        }
+        //all other conditions, highlighted shall be null
+        else{
             highlighted = null;
         }
     });
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -9,17 +9,26 @@ let comments = [{"Name" : "Miles Acosta",
                 "Date": "02/17/2021"}
 ];
 
+//using the getDate method to output the computer's date, we would have to create the function's inner workings
 function getDate(){
+    //assigning today as a new Date
     let today = new Date();
+    // let dd = the day it is in the month
     let dd = today.getDate();
+    //let mm = the month it is
     let mm = today.getMonth()+1;//this is due to index 0 being janurary
+    //let yyyy = the year it is
     let yyyy = today.getFullYear();
+    //conditionals for single digits so 1 would become 01
     if (dd<10){
         dd="0"+dd
     }if (mm<10){
         mm='0'+mm
-    }dateValue = mm + "/" + dd + "/" + yyyy;
+    }
+    //assigning dateValue to be a string containing month, date, year in the format mm/dd/yyyy
+    dateValue = mm + "/" + dd + "/" + yyyy;
 
+    //adding the value of the dateValue to id #comments__date
     document.getElementById("comments__date").value = dateValue;
 
     return dateValue;
@@ -27,35 +36,42 @@ function getDate(){
 
 getDate();
 
+//assigned form to the class .comments which is our form element in html
 const form =document.querySelector(".comments");
+//add event listener for submition, and preventDefault so it doesn't refresh
 form.addEventListener('submit',function(e){
     e.preventDefault();
 
+    //assign the values of the elements to constants
     const nameValue = e.target.Name.value;
     const commentValue = e.target.Comment.value;
 
+    //conditional so that if no content is filled, we wil change the border color to red and console.log please fill in field
     if(!nameValue && !commentValue){
         e.target.Name.style.borderColor ="#D22D2D";
         e.target.Comment.style.borderColor ="#D22D2D";
         console.log("Please fill in fields");
 
     }else{
+        //conditional so that if Name element form is not filled bordercolor will be red and returns false so that it doesn't continue and log
         if(!nameValue){;
             e.target.Name.style.borderColor ="#D22D2D";
             return false;
         }else{
             e.target.Name.style.borderColor ="";
         }
-
-    if(!commentValue){
-        e.target.Comment.style.borderColor ="#D22D2D";
-        return false;
-    }else{
-        e.target.Comment.style.borderColor ="";
+        //conditional so that if Comment element form is not filled bordercolor will be red and returns false so that it doesn't continue and log
+        if(!commentValue){
+            e.target.Comment.style.borderColor ="#D22D2D";
+            return false;
+        }else{
+            e.target.Comment.style.borderColor ="";
     }
 
+    //pushing all the values into the objects
     comments.push({Name:nameValue,Comment:commentValue,Date:dateValue});
 
+    renderComments();
     console.log(comments);
     console.log(e.target.Name.value);
     console.log(e.target.Comment.value);
@@ -63,42 +79,63 @@ form.addEventListener('submit',function(e){
     }
 });
 
+//assign footer to the footer in document
 const footer =document.querySelector('footer');
 
+//assign posts to a new div named .posts
 const posts = document.createElement("div");
 posts.classList.add("posts");
 
+//assigning body to the parent of footer, inserting .posts before footer in document
 const body = footer.parentNode;
 body.insertBefore(posts,footer);
 
-for (let i = 0; i < comments.length; i++) {
-    const comment = comments[i];
-  
-    const postsLeft = document.createElement('div');
-    postsLeft.classList.add("posts___left");
-    posts.appendChild(postsLeft);
-  
-    const postsRight = document.createElement('div');
-    postsRight.classList.add("posts___right");
-    posts.appendChild(postsRight);
-  
-    const posts__image = document.createElement("img");
-    posts__image.src = "https://www.colorhexa.com/e1e1e1.png";
-    posts__image.classList.add("posts__image");
-    postsLeft.appendChild(posts__image);
-  
-    const postsName = document.createElement('div');
-    postsName.classList.add("posts___name");
-    postsName.textContent = comment.Name;
-    postsRight.appendChild(postsName);
-  
-    const postsDate = document.createElement('div');
-    postsDate.classList.add("posts___date");
-    postsDate.textContent = comment.Date;
-    postsRight.appendChild(postsDate);
-  
-    const postsComment = document.createElement('div');
-    postsComment.classList.add("posts___comment");
-    postsComment.textContent = comment.Comment;
-    postsRight.appendChild(postsComment);
+function renderComments() {
+    posts.innerHTML = '';
+
+    //loop to start in the reverse order of Shows due to the latest being at the bottom
+    for (let i = comments.length -1; i >= 0; i--) {
+        const comment = comments[i];
+        
+        //creating element div and naming it .posts__section and appending it under posts
+        const postsSection = document.createElement('div');
+        postsSection.classList.add("posts__section");
+        posts.appendChild(postsSection)
+
+        //creating element div and naming it .post__left and appending it under post__section
+        const postsLeft = document.createElement('div');
+        postsLeft.classList.add("posts__left");
+        postsSection.appendChild(postsLeft);
+    
+        //creating element div and naming it .post__right and appending it under post__section
+        const postsRight = document.createElement('div');
+        postsRight.classList.add("posts__right");
+        postsSection.appendChild(postsRight);
+    
+        //creating img, naming it .posts__image and appening it under post__left
+        const posts__image = document.createElement("img");
+        posts__image.src = "https://www.colorhexa.com/e1e1e1.png";
+        posts__image.classList.add("posts__image");
+        postsLeft.appendChild(posts__image);
+    
+        //creating element div and naming it .post__name and appending it under post__right
+        const postsName = document.createElement('div');
+        postsName.classList.add("posts__name");
+        postsName.textContent = comment.Name;
+        postsRight.appendChild(postsName);
+    
+        ///creating element div and naming it .post__date and appending it under post__right
+        const postsDate = document.createElement('div');
+        postsDate.classList.add("posts__date");
+        postsDate.textContent = comment.Date;
+        postsRight.appendChild(postsDate);
+    
+        ///creating element div and naming it .post__comment and appending it under post__right
+        const postsComment = document.createElement('div');
+        postsComment.classList.add("posts__comment");
+        postsComment.textContent = comment.Comment;
+        postsRight.appendChild(postsComment);
+    }
 }
+
+renderComments();
